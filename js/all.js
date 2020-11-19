4c69aeeb314a1698f2978c59d622ed19f78e7c4b
yehyitung <joe66033166@gmail.com>
2020-11-20 02:42:27 +0800

    RWD(mobile)

--- js/all.js
+++ js/all.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
     $('html,body').animate({
       scrollTop: 0
     },500);
-  })
+  });
+  $('.show-menu').on('click',  function(e){
+    e.preventDefault();
+    $('body').toggleClass('menu-active');
+});
+
 });
 
 var openLightBox = document.querySelector('.works');
@@ -16,4 +21,37 @@ closeBtn.addEventListener('click', function(e){
   if(e.target.id === "lightbox"){
     document.querySelector('.lightbox').classList.remove("open");
   }
+});
+
+gsap.registerPlugin(ScrollTrigger);
+// gsap.defaults({ease:"none", duration:1});
+
+// const tl = gsap.timeline();
+// tl.from(".about-left-pic",{xPercent: -100})
+//   .from(".about-content",{xPercent: 100})
+
+
+// ScrollTrigger.create({
+//   animation:tl,
+//   trigger:"#container",
+//   star:"top center",
+//   end:"+=400",
+//   scrub:true,
+//   pin:true,
+//   anticipatePin:1
+// })
+gsap.from(".about-cat",{
+  scrollTrigger: {
+    trigger:".about-cat",
+    start:"0 100%",
+    end:"0 40%",
+    markers:true,
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  x:-800,
+  scaleX:.3,
+  scaleY:.3,
+  rotation: 360,
+  duration:3
 });
