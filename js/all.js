5b8fd9c65e906392c5fa82dc9e65498f1ab38cc0
yehyitung <joe66033166@gmail.com>
2020-12-01 17:52:31 +0800

    work-section-delete

--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
+// backtop-btn, RWD menu-btn
 $(document).ready(function () {
   $('backto-top a').click(function(e){
     e.preventDefault();
@@ -8,10 +9,10 @@ $(document).ready(function () {
   $('.show-menu').on('click',  function(e){
     e.preventDefault();
     $('body').toggleClass('menu-active');
+  });
 });
 
-});
-
+// lightbox open,close
 var openLightBox = document.querySelector('.works');
 openLightBox.addEventListener('click', function(){
   document.querySelector('.lightbox').classList.add("open");
@@ -23,29 +24,15 @@ closeBtn.addEventListener('click', function(e){
   }
 });
 
+// gsap animation effect
 gsap.registerPlugin(ScrollTrigger);
-// gsap.defaults({ease:"none", duration:1});
 
-// const tl = gsap.timeline();
-// tl.from(".about-left-pic",{xPercent: -100})
-//   .from(".about-content",{xPercent: 100})
-
-
-// ScrollTrigger.create({
-//   animation:tl,
-//   trigger:"#container",
-//   star:"top center",
-//   end:"+=400",
-//   scrub:true,
-//   pin:true,
-//   anticipatePin:1
-// })
 gsap.from(".about-cat",{
   scrollTrigger: {
     trigger:".about-cat",
     start:"0 100%",
     end:"0 40%",
-    markers:true,
+    // markers:true,
     toggleAction:"play reverse play reverse",
     scrub: true
   },
@@ -53,5 +40,102 @@ gsap.from(".about-cat",{
   scaleX:.3,
   scaleY:.3,
   rotation: 360,
+  duration:2
+});
+
+gsap.from(".about-content",{
+  scrollTrigger: {
+    trigger:".about-content",
+    start:"0 100%",
+    end:"0 40%",
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  x:1500,
+  duration:2
+});
+
+gsap.from(".works-img-cloud",{
+  scrollTrigger: {
+    trigger:".works-img-cloud",
+    start:"0 100%",
+    end:"0 40%",
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  x:100,
+  ease:"bounce",
   duration:3
 });
+
+gsap.from(".works-img-star",{
+  scrollTrigger: {
+    trigger:".works-img-star",
+    start:"0 100%",
+    end:"0 40%",
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  x:-200,
+  ease:"bounce",
+  duration:3
+});
+
+gsap.from(".works-img-rhombus",{
+  scrollTrigger: {
+    trigger:".works-img-rhombus",
+    start:"0 100%",
+    end:"0 40%",
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  y:150,
+  ease:"bounce",
+  duration:3
+});
+
+gsap.from(".works-img-trapezoid",{
+  scrollTrigger: {
+    trigger:".works-img-trapezoid",
+    start:"0 100%",
+    end:"0 40%",
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  x:100,
+  ease:"bounce",
+  duration:3
+});
+
+gsap.from(".works-img-twocircle",{
+  scrollTrigger: {
+    trigger:".works-img-twocircle",
+    start:"0 100%",
+    end:"0 40%",
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  x:-200,
+  ease:"bounce",
+  duration:3
+});
+
+gsap.from(".works-img-circle",{
+  scrollTrigger: {
+    trigger:".works-img-circle",
+    start:"0 100%",
+    end:"0 40%",
+    toggleAction:"play reverse play reverse",
+    scrub: true
+  },
+  y:150,
+  ease:"bounce",
+  duration:3
+});
+
+Draggable.create("#flycat", {type:"x,y", edgeResistance:0.65, bounds:".hero-section", inertia:true});
+Draggable.create("#butcat", {type:"x,y", edgeResistance:0.65, bounds:".hero-section", inertia:true});
+Draggable.create("#stantcat", {type:"x,y", edgeResistance:0.65, bounds:".hero-section", inertia:true});
+Draggable.create("#sitcat", {type:"x,y", edgeResistance:0.65, bounds:".hero-section", inertia:true});
+Draggable.create("#backcat", {type:"x,y", edgeResistance:0.65, bounds:".hero-section", inertia:true});
+// Draggable.create("#flycat", {type: "rotation", inertia: true});
